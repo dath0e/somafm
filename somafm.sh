00924ee2f3def6bc24feeaaefff23a8f8b7a33ae
dath0e <dath0e@teknik.io>
2017-10-18 02:30:46 -0400

    Update somafm.sh

--- somafm.sh
+++ somafm.sh
@@ -1,13 +1,5 @@
 #!/bin/sh
-#
-# Simple cli for somafm.
-# Requires cvlc and curl.
-# By default plays 128k AAC, you can easily change.
-# Saves the scraped channels in channels.soma
-# 
-# Would be nice to add...
-# Channel info
-# Channel error check
+
 
 function depend {
 	curl -V &> /dev/null 
